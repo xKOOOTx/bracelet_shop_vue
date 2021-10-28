befb5a95b244de1c5c6be096e6ba7668419534c0
xKOOOTx <kooot123@gmail.com>
2021-10-28 22:56:27 +0300

    made Header.vue

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,39 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view/>
   </div>
 </template>
 
 <style lang="scss">
+@import './style/variables';
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Ubuntu, Cantarell,
+                "Helvetica Neue", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  color: $fontColor;
 }
 
-#nav {
-  padding: 30px;
+// main
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+a {
+  text-decoration: none;
+  &:hover {
+    text-decoration: underline;
   }
+}
+
+li {
+  list-style: none;
+}
+
+img {
+  display: block;
 }
 </style>
